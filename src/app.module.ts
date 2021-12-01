35485528d900749a9507e2fbea2db2936f2cd612
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-12-01 23:04:34 +0545

    feat: init

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ExpenseModule } from './expense/expense.module';
+import { SalesModule } from './sales/sales.module';
+import { BillsModule } from './bills/bills.module';
+import { FirmModule } from './firm/firm.module';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    UserModule,
+    MikroOrmModule.forRoot({
+      entities: ['./dist/entities'],
+      entitiesTs: ['./src/entities'],
+      dbName: 'test',
+      type: 'mysql',
+    }),
+    ExpenseModule,
+    SalesModule,
+    BillsModule,
+    FirmModule,
+    ProductsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
